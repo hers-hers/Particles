c92962f50a9c9e818de3eddff58d46aaffeb6574
Shong Her <W1168037@apps.losrios.edu>
2024-12-09 23:37:06 -0800

    12/9/24 @ 11:36pm

--- Engine.cpp
+++ Engine.cpp
@@ -5,7 +5,7 @@
 ////////////////////////////////////////////////////////////////
 
 Engine::Engine() {
-	
+	m_Window.create(VideoMode::getDesktopMode(), "Particle Engine");
 }
 
 
@@ -18,7 +18,24 @@ void Engine::run() {
 
 
 void Engine::input() {
+	Event event;
+	while (m_Window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			m_Window.close();
+		}
+		if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
+			m_Window.close();
+		}
+		if (event.type == Event::MouseButtonPressed &&
+			event.mouseButton.button == Mouse::Left) {
+			Vector2i mouseClickPosition = Mouse::getPosition(m_Window);
 
+			for (int i = 0; i < 5; i++) {
+				int numPoints = 25 + (rand() % 50);
+				m_particles.emplace_back(m_Window, numPoints, mouseClickPosition);
+			}
+		}
+	}
 }
 
 
@@ -26,7 +43,14 @@ void Engine::input() {
 
 
 void Engine::update(float dtAsSeconds) {
-
+	for (auto up = m_particles.begin(); up != m_particles.end();) {
+		up->update(dtAsSeconds);
+		if (up->getTTL() > 0.0) {
+			up->update(dtAsSeconds);
+			up++;
+		}
+		else { up = m_particles.erase(up); }
+	}
 }
 
 
@@ -34,5 +58,9 @@ void Engine::update(float dtAsSeconds) {
 
 
 void Engine::draw() {
-
+	m_Window.clear();
+	for (auto& particle : m_particles) {
+		m_Window.draw(particle);
+	}
+	m_Window.display();
 }
--- Particle.cpp
+++ Particle.cpp
@@ -151,46 +151,95 @@ void Particle::unitTests()
 ////////////////////////////////////////////////////////////////
 
 
-Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition) {
+Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition) 
+    : m_A(2,numPoints), m_ttl(TTL), m_numPoints(numPoints), m_radiansPerSec((float)rand() / RAND_MAX * M_PI)
+{
+    m_cartesianPlane.setCenter(0, 0);
+    m_cartesianPlane.setSize(target.getSize().x,  (- 1.0) * target.getSize().y);
+
+    m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
+
+    m_vx = (rand() % 2 == 0 ? 1 : -1) * ((float)rand() / RAND_MAX * 500 + 100);
+    m_vy = ((float)rand() / RAND_MAX * 500 + 100);
+
+    m_color1 = Color::White;
+    m_color2 = Color(rand() % 256, rand() % 256, rand() % 256);
+
+    double theta = 0;
+    double dTheta = 2 * M_PI / (numPoints - 1);
+    m_A = Matrix(2, numPoints);
+
+    for (int j = 0; j < numPoints; j++) {       // prof said to use j here ?
+        double r = (rand() % 80 + 20);
+        double dx = r * cos(theta);
+        double dy = r * sin(theta);
+        m_A(0, j) = m_centerCoordinate.x + dx;
+        m_A(1, j) = m_centerCoordinate.y + dy;
+        theta += dTheta;
+    }
 
 }
 
 
 void Particle::draw(RenderTarget& target, RenderStates states) const {
+    VertexArray lines(TriangleFan, m_numPoints + 1);
+    Vector2f center = target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane);
+    lines[0].position = center;
+    lines[0].color = m_color1;
 
+    for (int j = 1; j <= m_numPoints; j++) {
+        lines[j].position = target.mapCoordsToPixel(m_A(1, j), m_cartesianPlane);
+        lines[j].color = m_color2;
+    }
+    target.draw(lines);
 }
 
 
 
 void Particle::update(float dt) {
-
+    m_ttl -= dt;
+    rotate(dt * m_radiansPerSec);
+    scale(SCALE);
+    float dx = m_vx * dt;
+    m_vy -= G * dt;
+    float dy = m_vy * dt;
+    translate(dx, dy);
 }
 
 
 void Particle::translate(double xShift, double yShift) {
-
+    TranslationMatrix T(xShift, yShift, m_A.getCols());
+    m_A = T + m_A;
+    m_centerCoordinate.x += xShift;
+    m_centerCoordinate.y += yShift;
 }
 
 
 
 void Particle::rotate(double theta) {
-
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    RotationMatrix R(theta);
+    m_A = R * m_A;
+    translate(temp.x, temp.y);
 }
 
 
 
 void Particle::scale(double c) {
-
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    ScalingMatrix S(c);
+    m_A = S * m_A;
+    translate(temp.x, temp.y);
 }
 
 
-
+/*
 bool Particle::almostEqual(double a, double b, double eps = 0.0001) {
 
 }
-
-
-
 void Particle::unitTests() {
 
 }
+*/  // these two functions have been defined by prof at top
